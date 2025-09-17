202f349c750e6c8e7d5d41636304e97eac0f88a8
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-09-17 21:04:57 +0100

    First Commit, Just create a Django project and Django App... Made some basic changes (like providing template folder in Django setting, static folder and model creation

---
+++ portfolio/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+
+# Biography Model
+class Biography(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    profile_image = models.ImageField(upload_to='profiles/')
+    phone = models.CharField(max_length=20, blank=True)
+    
+
+    def __str__(self):
+        return self.name
+    
+# Service Model
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    icon = models.CharField(max_length=100)  # You can store icon class names or paths
+
+    def __str__(self):
+        return self.title
+    
+# Logos Model
+class Logos(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='logos/')
+
+    def __str__(self):
+        return self.name
+
+# Project Model
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/')
+    link = models.URLField(max_length=200, blank=True)
+    github_link = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+class Achievement(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    position = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    challenge_name = models.CharField(max_length=200)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+    
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    proficiency = models.IntegerField()  # Percentage from 0 to 100
+
+    def __str__(self):
+        return self.name
+
+# Mails Model
+class Email(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, blank=False)
+    message = models.TextField()
+
+class AboutWork(models.Model):
+    title = models.CharField(max_length=200)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+class ExtraText(models.Model):
+    text = models.TextField()
+    icon = models.CharField(max_length=100)  # You can store icon class names or paths_
+
+    def __str__(self):
+        return self.text[:50]  # Return first 50 characters for representation
+    
+class HeadingText(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
---
+++ static/js/script.js
@@ -0,0 +1,110 @@
+/* 
+**************************
+MAKING THE ACCORDION WORK
+**************************
+*/
+
+const div_service_description = document.querySelectorAll(
+  ".service_description"
+);
+const div_service_header = document.querySelectorAll(".service_header");
+
+div_service_header.forEach((header) => {
+  header.addEventListener("click", () => {
+    // Toggle the change_icon class on the header
+    header.classList.toggle("change_icon");
+
+    // Get the parent element of the header, which is the service_detail
+    const service_detail = header.parentElement;
+    const service_description = service_detail.querySelector(
+      ".service_description"
+    );
+
+    // Toggle the active class on the service_description
+    service_description.classList.toggle("active");
+
+    // if (service_description.classList.contains("active"))
+  });
+});
+
+/* 
+**************************
+ADDING A STICKY NAVBAR
+**************************
+*/
+const section_hero_el = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (ent.isIntersecting == false) document.body.classList.add("sticky");
+
+    if (ent.isIntersecting == true) document.body.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(section_hero_el);
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const all_links = document.querySelectorAll(".main-header-link");
+all_links.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scrool back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const section_el = document.querySelector(href);
+      section_el.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+///////////////////////////////////////////////////////////
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+///////////////////////////////////////////////////////////
+
+// HOW TO MAKE THE NAVIGATION WORK
+const btnNavEl = document.querySelector(".btn_mobile_nav");
+const headerEl = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
