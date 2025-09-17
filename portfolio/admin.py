b2261380deda9abe9650ca69867f00194dae6564
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-09-17 21:28:41 +0100

    Register my Models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Biography, Service, Logos, Project, Achievement, Skill, HeadingText, Email, ExtraText
 
 # Register your models here.
+admin.site.register(Biography)
+admin.site.register(Service)
+admin.site.register(Logos)
+admin.site.register(Project)
+admin.site.register(Achievement)
+admin.site.register(Skill)
+admin.site.register(HeadingText)
+admin.site.register(Email)
+admin.site.register(ExtraText)
