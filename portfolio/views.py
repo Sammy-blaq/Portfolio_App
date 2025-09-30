9b3dfd7a3b0b5dddbb4feca698f1b68f84da2865
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-09-30 02:19:53 +0100

    Added the form and validate its input and stord the cleaned data to the model

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Email
+
+from . forms import EmailForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'portfolio/index.html')
+
+    form = EmailForm()
+
+    context = {
+        'form': form,
+    }
+
+    if (request.method == 'POST'):
+
+        # Validating Email and input data
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            mail_data = Email.objects.create(name=name, email=email, message=message)
+            mail_data.save()
+
+            return redirect('index'),  # Redirect to the same page to show the success message
+    
+    return render(request, 'portfolio/index.html', context=context)
