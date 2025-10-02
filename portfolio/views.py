0ddc7699c91c99841c5ab8523786e53d196266bf
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-10-02 14:00:12 +0100

    Import my Email form and model data, passed my model data to the context, to enable it to be used in the template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Email
 
+# Importing my Models
+from .models import Email, Project, Skill, Service, SuccessStory, AboutWork, Achievement, HeadingText, ExtraText, Logos, Biography, SocialLink
+
+# Importing Email Form
 from . forms import EmailForm
 
 # Create your views here.
@@ -9,8 +11,37 @@ def index(request):
 
     form = EmailForm()
 
+
+    # Getting my model data
+    projects = Project.objects.all()
+    biography = Biography.objects.first()
+    main_heading = HeadingText.objects.first()
+    services = Service.objects.all()
+    logos = Logos.objects.all()
+    skils = Skill.objects.all()
+    social_links = SocialLink.objects.all()[:4] # Get the first 4 social links
+    footer_social_links = SocialLink.objects.all()[4:] # Get the remaining social links
+    success_stories = SuccessStory.objects.all()
+    success_story_text = ExtraText.objects.last()
+    services_text = ExtraText.objects.first()
+    about_work = AboutWork.objects.all()
+    achievements = Achievement.objects.all()
+
     context = {
         'form': form,
+        'projects': projects,
+        'biography': biography,
+        'main_heading': main_heading,
+        'services': services,
+        'logos': logos,
+        'skills': skils,
+        'social_links': social_links,
+        'footer_social_links': footer_social_links,
+        'success_stories': success_stories,
+        'success_story_text': success_story_text,
+        'services_text': services_text,
+        'about_work': about_work,
+        'achievements': achievements,
     }
 
     if (request.method == 'POST'):
