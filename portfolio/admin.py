32f42d5a8ced084077610d7e4c2f3d8e7d78714c
Sammy-blaq <abdullahiabdussamad00@gmail.com>
2025-10-02 14:39:55 +0100

    I added Model Admin class that modify the display of each model in the admin page

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,29 +1,55 @@
 from django.contrib import admin
-from .models import Biography, Service, Logos, Project, Achievement, Skill, HeadingText, Email, ExtraText, AboutWork, SuccessStory
+from .models import Biography, Service, Logos, Project, Achievement, Skill, HeadingText, Email, ExtraText, AboutWork, SuccessStory, SocialLink
 
 class ProjectAdmin(admin.ModelAdmin):
+  # Define field under a Pre-defined Title
   fieldsets = [
     (None, {'fields': ['title', 'category', 'description', 'image']}),
     ('More details', {'fields': ['date', 'link', 'github_link']})
+  ]
+
+  list_display = [
+    'title',
+    'category',
+    'description',
+    'date',
   ]
 
 class EmailAdmin(admin.ModelAdmin):
   list_display = [
     'name',
     'email',
-    'message'
+    'message',
+    'date'
   ]
 
   # add a filter by email
   list_filter = [
-    'email'
+    'email',
+    'date',
   ]
 
   # add a search field by name
   search_fields = [
-    'name'
+    'name',
+    'email',
   ]
   
+
+class SocialLinkAdmin(admin.ModelAdmin):
+  list_display = [
+    'platform',
+    'url',
+    'icon'
+  ]
+
+class SuccssStoryAdmin(admin.ModelAdmin):
+  # Defines the field to display
+  list_display = [
+    'success_count',
+    'success_description',
+  ]
+
 
 # Register your models here.
 admin.site.register(Biography)
@@ -36,4 +62,5 @@ admin.site.register(HeadingText)
 admin.site.register(Email, EmailAdmin)
 admin.site.register(ExtraText)
 admin.site.register(AboutWork)
-admin.site.register(SuccessStory)
+admin.site.register(SuccessStory, SuccssStoryAdmin)
+admin.site.register(SocialLink, SocialLinkAdmin)
